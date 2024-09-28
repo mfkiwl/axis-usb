b3f14401ae30aec2441b6b5ecfe39354fddc3113
Patrick Suggate <patrick.suggate@gmail.com>
2024-09-28 20:16:07 +1200

    Chore.

--- lib/misc-verilog-cores.bash
+++ lib/misc-verilog-cores.bash
@@ -4,10 +4,16 @@ TMP=`mktemp --directory`
 pushd ${TMP}
 git clone --depth=1 https://github.com/psuggate/misc-verilog-cores.git
 cd misc-verilog-cores
-git checkout 4c85a3051c88b1aa3ccfdd3ffbfda6ff63334f00
+git checkout 6c9d67a6b10024b72b8e76aa474b91f9babd6280
 popd
 mkdir -p misc-verilog-cores
-cp -a ${TMP}/misc-verilog-cores/rtl/* misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/arch misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/axis misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/fifo misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/misc misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/spi  misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/uart misc-verilog-cores/
+cp -a ${TMP}/misc-verilog-cores/rtl/Makefile misc-verilog-cores/
 cp -a ${TMP}/misc-verilog-cores/*.md misc-verilog-cores/
 cp -a ${TMP}/misc-verilog-cores/driver misc-verilog-cores/
 echo "removing temp directory ${TMP}"
